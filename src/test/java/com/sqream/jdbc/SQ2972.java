1faa0feb2bc873f3ed9cc89e7a9002048e666fba
Alex Karpov <alexk@sqreamtech.com>
2020-05-21 09:23:34 +0300

    Do insert simultaneously

--- src/test/java/com/sqream/jdbc/SQ2972.java
+++ src/test/java/com/sqream/jdbc/SQ2972.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.sqream.jdbc.TestEnvironment.createConnection;
 
@@ -26,8 +28,19 @@ public class SQ2972 {
              PreparedStatement pstmt1 = conn1.prepareStatement("insert into test_table_1 values (?);");
              PreparedStatement pstmt2 = conn2.prepareStatement("insert into test_table_2 values (?);")) {
 
-            pstmt1.setInt(1, 1);
-            pstmt2.setInt(1, 1);
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            executorService.submit(() -> setTestValue(pstmt1));
+            executorService.submit(() -> setTestValue(pstmt2));
+        }
+    }
+
+    private void setTestValue(PreparedStatement pstmt) {
+        try {
+            pstmt.setInt(1, 1);
+            pstmt.addBatch();
+            pstmt.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
