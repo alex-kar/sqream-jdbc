6a6db66820758531ffc91a808a83ed4ef8f2e1a8
Alex Karpov <alexk@sqreamtech.com>
2020-05-31 10:17:14 +0300

    Create additional test for ResultSetMetaData#getColumnDisplaySize()

--- src/test/java/com/sqream/jdbc/SQResultSetMetaDataTest.java
+++ src/test/java/com/sqream/jdbc/SQResultSetMetaDataTest.java
@@ -2,8 +2,16 @@ package com.sqream.jdbc;
 
 import com.sqream.jdbc.connector.ConnException;
 import com.sqream.jdbc.connector.Connector;
+import com.sqream.jdbc.connector.ConnectorFactory;
 import com.sqream.jdbc.connector.ConnectorImpl;
+import com.sqream.jdbc.connector.socket.SQSocket;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 import java.security.KeyManagementException;
@@ -12,7 +20,10 @@ import java.sql.*;
 
 import static com.sqream.jdbc.TestEnvironment.*;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConnectorFactory.class, SQSocket.class})
 public class SQResultSetMetaDataTest {
 
     @Test
@@ -58,5 +69,44 @@ public class SQResultSetMetaDataTest {
         assertEquals(Integer.MAX_VALUE, rsmeta.getColumnDisplaySize(12));
         assertEquals(10, rsmeta.getColumnDisplaySize(13));
         assertEquals(23, rsmeta.getColumnDisplaySize(14));
+    }
+
+    /**
+     * To get column size driver does additional request to server.
+     * That request should be done before close statement.
+     * Otherwise when we call ResultSetMetaData#getColumnDisplaySize() server can be blocked with another statement.
+     */
+    @Test
+    public void columnDisplaySizeDoesNotBlockServerTest() throws ConnException, SQLException {
+        String createSql = "create or replace table test_display_size " +
+                "(col1 bool, col2 tinyint, col3 smallint, col4 int, col5 bigint, col6 real, col7 double, " +
+                "col8 varchar(10), col9 nvarchar(10), col10 text(10), col11 nvarchar, col12 text, col13 date, col14 datetime)";
+        String selectSql = "select * from test_display_size";
+        int expectedColumnCount = 14;
+
+        ResultSetMetaData rsmeta;
+        try (Connection conn = createConnection();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute(createSql);
+            ResultSet rs = stmt.executeQuery(selectSql);
+            rsmeta = rs.getMetaData();
+            rs.close();
+        }
+
+        Connector connectorMock = Mockito.mock(Connector.class);
+        PowerMockito.mockStatic(ConnectorFactory.class);
+        PowerMockito.when(ConnectorFactory.initConnector(IP, PORT, CLUSTER, SSL)).thenReturn(connectorMock);
+        SQSocket socketMock = Mockito.mock(SQSocket.class);
+        PowerMockito.mockStatic(SQSocket.class);
+        PowerMockito.when(SQSocket.connect(IP, PORT, SSL)).thenReturn(socketMock);
+
+        assertEquals(expectedColumnCount, rsmeta.getColumnCount());
+        for (int i = 0; i < rsmeta.getColumnCount(); i++) {
+            rsmeta.getColumnDisplaySize(i + 1);
+        }
+
+        Mockito.verify(connectorMock, VerificationModeFactory.times(0)).connect(any(), any(), any(), any());
+        Mockito.verify(socketMock, VerificationModeFactory.times(0)).read(any());
     }
 }
